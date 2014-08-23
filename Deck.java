80c737d2f848aa6fccb7b92abf76e05d1ffaef8d
Chris <chrisjeng@berkeley.edu>
2014-08-23 12:50:59 -0700

    Basic components roughly sketched out.

--- Deck.java
+++ Deck.java
@@ -1,4 +1,40 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Deck {
+
+	public static final int SIZE = 52;
+
+	private HashSet<Card> drawnCards;
+	private Queue<Card> myCards;
+
+	// Initializes a shuffled deck
+	public Deck() {
+		this.drawnCards = new HashSet<Card>(SIZE);
+		this.myCards = new LinkedList<Card>();
+	}
+
+	public static Deck makeDeck() {
+		// TODO
+		HashSet<Integer> addedNumbers = new HashSet<Integer>();
+		int rand52 = (int) (Math.random() * 52 + 1);
+		return null;
+	}
+
+	// Draws a random card
+	public Card draw() {
+		Card toRtn = myCards.poll();
+		drawnCards.add(toRtn);
+		return toRtn;
+	}
+
+	// Returns whether or not the collection contains
+	public boolean contains(Card c) {
+		return false;
+	}
+
+	public void shuffle() {
+	}
 
 }
---
+++ Player.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+//TODO: implement moods
+public class Player {
+
+	private int cents; // in cents
+	private String NAME;
+	private Hand myHand;
+	
+	public Player(int money, String name, Deck sharedDeck) {
+		this.cents = money;
+		this.NAME = name;
+		myHand = new Hand(sharedDeck);
+	}
+	
+	public int getCents() {
+		return cents;
+	}
+	
+	public Card c1() {
+		return myHand.c1;
+	}
+	
+	public Card c2() {
+		return myHand.c2;
+	}
+	
+	private class Hand {
+		
+		private Deck sharedDeck;
+		private Card c1;
+		private Card c2;
+		
+		public Hand(Deck d) {
+			this.sharedDeck = d;
+		}
+		
+		public void drawFromDeck() {
+			this.c1 = sharedDeck.draw();
+			this.c2 = sharedDeck.draw();
+		}
+	}
+}
